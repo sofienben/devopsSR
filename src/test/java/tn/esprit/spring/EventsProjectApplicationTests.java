2091e9a4e3401329dd94dc3190f39bdf03890712
sofienben <123633410+sofienben@users.noreply.github.com>
2024-12-12 08:15:25 +0100

    Update EventsProjectApplicationTests.java

--- src/test/java/tn/esprit/spring/EventsProjectApplicationTests.java
+++ src/test/java/tn/esprit/spring/EventsProjectApplicationTests.java
@@ -1,12 +1,31 @@
-package tn.esprit.spring;
+// package tn.esprit.spring;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class EventsProjectApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.services.EventService;
 
 @SpringBootTest
 class EventsProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private EventService eventService;
 
+    @Test
+    void contextLoads() {
+        assertThat(eventService).isNotNull();
+    }
 }
+
